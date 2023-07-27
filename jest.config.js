52d85d56db92cd597b8602c4564ede9a43ae1d73
aleksandersousa <aleksander-ssousa@outlook.com>
2023-07-27 17:41:51 -0300

    chore: add jest config and sripts

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ const config = {
   // Set root directory
   roots: ['<rootDir>/src'],
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  collectCoverage: false,
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   // The directory where Jest should output its coverage files
