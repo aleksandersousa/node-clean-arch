1938168b191443d653066d2b2758282206fb112c
aleksandersousa <aleksander-ssousa@outlook.com>
2023-07-28 13:01:57 -0300

    test: ensure BCryptAdapter throw if bcrypt throws

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
 
       rules: {
         '@typescript-eslint/strict-boolean-expressions': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
         'prettier/prettier': [
           'error',
           {
--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -28,7 +28,15 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut();
 
     const hash = await sut.encrypt('any_value');
-
     expect(hash).toBe('hash');
+  });
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut();
+
+    jest.spyOn(bcrypt, 'hash').mockRejectedValueOnce(new Error() as never);
+
+    const promise = sut.encrypt('any_value');
+    await expect(promise).rejects.toThrow();
   });
 });
