697c9fb78dc5fedc4623540afc4fdac6e252b0f3
aleksandersousa <aleksander-ssousa@outlook.com>
2023-07-29 10:08:09 -0300

    test: add Signup Routes integration tests

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import app from '../config/app';
+
+describe('Signup Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({ name: 'Aleksander', email: 'aleksander@email.com', password: '123', passwordConfirmation: '123' })
+      .expect(200);
+  });
+});
