23a679a32fa8580abb000da75562d0a8482c101c
aleksandersousa <aleksander-ssousa@outlook.com>
2023-07-31 16:08:00 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -5,8 +5,18 @@ describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('any_field');
 
-    const error = sut.validate({ any_other_filed: 'any_other_filed' });
+    const error = sut.validate({ any_other_field: 'any_other_field' });
 
     expect(error).toEqual(new MissingParamError('any_field'));
+  });
+});
+
+describe('RequiredField Validation', () => {
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('any_field');
+
+    const error = sut.validate({ any_field: 'any_other_field' });
+
+    expect(error).toBeFalsy();
   });
 });
