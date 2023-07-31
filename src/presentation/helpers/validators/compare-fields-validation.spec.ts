6596b54193bf63a23ac5610f2e8ca39c794e01fb
aleksandersousa <aleksander-ssousa@outlook.com>
2023-07-31 16:16:19 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,24 @@
+import { InvalidParamError } from '../../errors';
+import { CompareFieldsValidation } from './compare-fields-validation';
+
+const makeSut = (): CompareFieldsValidation => new CompareFieldsValidation('any_field', 'field_to_compare');
+
+describe('CompareFields Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut();
+
+    const error = sut.validate({ any_field: 'any_field', field_to_compare: 'differente_value' });
+
+    expect(error).toEqual(new InvalidParamError('field_to_compare'));
+  });
+});
+
+describe('CompareFields Validation', () => {
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut();
+
+    const error = sut.validate({ any_field: 'any_field', field_to_compare: 'any_field' });
+
+    expect(error).toBeFalsy();
+  });
+});
