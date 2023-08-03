687aec59eb7a754e9d085fd054a9e650b9ec729d
aleksandersousa <aleksander-ssousa@outlook.com>
2023-08-03 09:38:17 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -161,4 +161,12 @@ describe('DbAuthentication', () => {
 
     expect(promise).rejects.toThrow();
   });
+
+  test('Should return a token on success', async () => {
+    const { sut } = makeSut();
+
+    const accessToken = await sut.auth(makeFakeAuthentication());
+
+    expect(accessToken).toBe('any_token');
+  });
 });
