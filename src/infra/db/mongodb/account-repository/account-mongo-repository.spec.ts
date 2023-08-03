497a113d2ffc124523f75e57ec3d85af0a5c848a
aleksandersousa <aleksander-ssousa@outlook.com>
2023-08-03 13:12:59 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
@@ -1,9 +1,12 @@
+import { type Collection } from 'mongodb';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { AccountMongoRepository } from './account-mongo-repository';
 
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository();
 };
+
+let accountCollection: Collection;
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -15,11 +18,11 @@ describe('Account Mongo Repository', () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts');
+    accountCollection = await MongoHelper.getCollection('accounts');
     accountCollection.deleteMany({});
   });
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const sut = makeSut();
     const account = await sut.add({ name: 'any_name', email: 'any_email@mail.com', password: 'any_password' });
 
@@ -28,5 +31,18 @@ describe('Account Mongo Repository', () => {
     expect(account.name).toBe('any_name');
     expect(account.email).toBe('any_email@mail.com');
     expect(account.password).toBe('any_password');
+  });
+
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut();
+    await accountCollection.insertOne({ name: 'any_name', email: 'any_email@email.com', password: 'any_password' });
+
+    const account = await sut.loadByEmail('any_email@email.com');
+
+    expect(account).toBeTruthy();
+    expect(account?.id).toBeTruthy();
+    expect(account?.name).toBe('any_name');
+    expect(account?.email).toBe('any_email@email.com');
+    expect(account?.password).toBe('any_password');
   });
 });
