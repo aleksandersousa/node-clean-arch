4418974b95cad2746151fea45489f3dd6b2036ce
aleksandersousa <aleksander-ssousa@outlook.com>
2023-08-03 09:50:40 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,16 +2,19 @@ import { type AuthenticationModel, type Authentication } from '../../../domain/u
 import { type HashComparer } from '../../protocols/cryptography/hash-comparer';
 import { type TokenGenerator } from '../../protocols/cryptography/token-generator';
 import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
+import { type UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository';
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
     private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository,
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.hashComparer = hashComparer;
     this.tokenGenerator = tokenGenerator;
+    this.updateAccessTokenRepository = updateAccessTokenRepository;
   }
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
@@ -22,6 +25,7 @@ export class DbAuthentication implements Authentication {
 
       if (isPasswordValid) {
         const accessToken = await this.tokenGenerator.generate(account.id);
+        await this.updateAccessTokenRepository.update(account.id, accessToken);
         return accessToken;
       }
     }
