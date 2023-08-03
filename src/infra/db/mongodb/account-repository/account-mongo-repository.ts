bff55c996871792ac188da9aef190d14d99be82e
aleksandersousa <aleksander-ssousa@outlook.com>
2023-08-03 13:12:44 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.ts
@@ -1,10 +1,10 @@
 import { ObjectId } from 'mongodb';
-import { type AddAccountRepository } from '../../../../data/protocols';
+import { type LoadAccountByEmailRepository, type AddAccountRepository } from '../../../../data/protocols';
 import { type AccountModel } from '../../../../domain/models';
 import { type AddAccountModel } from '../../../../domain/usecases';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
 
@@ -15,5 +15,12 @@ export class AccountMongoRepository implements AddAccountRepository {
     );
 
     return MongoHelper.parseDocument<AccountModel>(document);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne({ email });
+
+    return MongoHelper.parseDocument<AccountModel>(account);
   }
 }
