71aba2386a2162c7fd904573248a23f9ff17b183
aleksandersousa <aleksander-ssousa@outlook.com>
2023-08-29 17:29:35 -0300

    test: ensure AddSurveyController returns noContent on success

--- src/presentation/controllers/survey/add-survey/add-survey.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey.spec.ts
@@ -1,6 +1,6 @@
 import { type Validation, type HttpRequest, type AddSurvey, type AddSurveyModel } from '.';
 import { MissingParamError } from '../../../errors';
-import { badRequest, serverError } from '../../../helpers/http/http-helper';
+import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper';
 import { AddSurveyController } from './add-survey-controller';
 
 const makeValidation = (): Validation => {
@@ -90,5 +90,13 @@ describe('AddSurvey Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(noContent());
   });
 });
