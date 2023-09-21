a45eb9eefea3c386f1da2ff53821d8219f6e4233
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-21 17:01:10 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,27 +2,23 @@ import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
 
 jest.mock('jsonwebtoken', () => ({
-  sign: async (): Promise<string> => {
-    return await new Promise(resolve => {
-      resolve('any_token');
-    });
+  async sign(): Promise<string> {
+    return 'any_token';
+  },
+
+  async verify(): Promise<string> {
+    return 'any_value';
   },
 }));
 
-interface SutTypes {
-  sut: JwtAdapter;
-}
-
-const makeSut = (): SutTypes => {
-  const sut = new JwtAdapter('secret');
-
-  return { sut };
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret');
 };
 
 describe('Jwt Adapter', () => {
   describe('sign()', () => {
     test('Should call sign with correct values', async () => {
-      const { sut } = makeSut();
+      const sut = makeSut();
       const signSpy = jest.spyOn(jwt, 'sign');
 
       await sut.encrypt('any_id');
@@ -31,7 +27,7 @@ describe('Jwt Adapter', () => {
     });
 
     test('Should return a token on sign success', async () => {
-      const { sut } = makeSut();
+      const sut = makeSut();
 
       const accessToken = await sut.encrypt('any_id');
 
@@ -39,12 +35,31 @@ describe('Jwt Adapter', () => {
     });
 
     test('Should throw if sign throws', async () => {
-      const { sut } = makeSut();
+      const sut = makeSut();
 
       jest.spyOn(jwt, 'sign').mockRejectedValueOnce(new Error() as never);
 
       const promise = sut.encrypt('any_id');
       await expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, 'verify');
+
+      await sut.decrypt('any_token');
+
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
+    });
+
+    test('Should return a value on verify success', async () => {
+      const sut = makeSut();
+
+      const value = await sut.decrypt('any_token');
+
+      expect(value).toBe('any_value');
     });
   });
 });
