ff9a90078604532f9e22d6b13217b428100b797f
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-22 00:38:08 -0300

    feat: ensure LoadSurveysController returns 200 in success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,13 +1,11 @@
 import { type LoadSurveys, type Controller, type HttpRequest, type HttpResponse } from '.';
+import { ok } from '../../../helpers/http/http-helper';
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load();
-    return {
-      body: 'any_body',
-      statusCode: 200,
-    };
+    const surveys = await this.loadSurveys.load();
+    return ok(surveys);
   }
 }
