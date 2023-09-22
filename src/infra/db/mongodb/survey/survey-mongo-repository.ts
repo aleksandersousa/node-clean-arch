89ef50550f4b19a31c5cdfb7f4188bd745bfacf6
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-22 12:23:04 -0300

    feat: ensure SurveyMongoRepository load all Surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,11 +1,19 @@
-import { type AddSurveyRepository } from '../../../../data/protocols';
+import { type LoadSurveysRepository, type AddSurveyRepository } from '../../../../data/protocols';
+import { type SurveyModel } from '../../../../domain/models';
 import { type AddSurveyModel } from '../../../../domain/usecases';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
 
     await surveyCollection.insertOne(surveyData);
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const surveys = await surveyCollection.find().toArray();
+
+    return surveys.map(survey => MongoHelper.parseDocument<SurveyModel>(survey)) as SurveyModel[];
   }
 }
