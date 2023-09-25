cc8af27a480521bd59e585c1b6b07c4be0b3bac2
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-25 18:15:11 -0300

    test: ensure SaveSurveyResult returns 200 when valid token is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,25 +1,27 @@
 import request from 'supertest';
 import app from '@/main/config/app';
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
+import { type Collection } from 'mongodb';
 
-// let surveyCollection: Collection;
-// let accountCollection: Collection;
+let surveyCollection: Collection;
+let accountCollection: Collection;
 
-// const makeAccessToken = async (): Promise<string> => {
-//   const res = await accountCollection.insertOne({
-//     name: 'any_name',
-//     email: 'any_email@email.com',
-//     password: '12345',
-//     role: 'admin',
-//   });
-//   const id = res.insertedId;
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@email.com',
+    password: '12345',
+  });
+  const id = res.insertedId;
 
-//   const accessToken = sign({ id }, env.jwtSecret);
+  const accessToken = sign({ id }, env.jwtSecret);
 
-//   await accountCollection.updateOne({ _id: id }, { $set: { accessToken } });
+  await accountCollection.updateOne({ _id: id }, { $set: { accessToken } });
 
-//   return accessToken;
-// };
+  return accessToken;
+};
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -30,13 +32,13 @@ describe('Survey Routes', () => {
     await MongoHelper.disconnect();
   });
 
-  // beforeEach(async () => {
-  //   surveyCollection = await MongoHelper.getCollection('surveys');
-  //   await surveyCollection.deleteMany({});
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
 
-  //   accountCollection = await MongoHelper.getCollection('accounts');
-  //   await accountCollection.deleteMany({});
-  // });
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
 
   describe('PUT /surveys/:surveyId/results', () => {
     test('Should return 403 on add survey without accessToken', async () => {
@@ -48,17 +50,21 @@ describe('Survey Routes', () => {
         .expect(403);
     });
 
-    // test('Should return 204 on add survey with valid token', async () => {
-    //   const accessToken = await makeAccessToken();
+    test('Should return 200 on save survey with valid token', async () => {
+      const accessToken = await makeAccessToken();
+      const surveyInsertedDocument = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{ image: 'http://image-name.com', answer: 'Answer 1' }, { answer: 'Answer 2' }],
+        date: new Date(),
+      });
 
-    //   await request(app)
-    //     .post('/api/surveys')
-    //     .set('x-access-token', accessToken)
-    //     .send({
-    //       question: 'Question',
-    //       answers: [{ image: 'http://image-name.com', answer: 'Answer 1' }, { answer: 'Answer 2' }],
-    //     })
-    //     .expect(204);
-    // });
+      await request(app)
+        .put(`/api/surveys/${surveyInsertedDocument.insertedId.toHexString()}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1',
+        })
+        .expect(200);
+    });
   });
 });
