fe1a259372c328573eb5e623b3087c649b6a66fc
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-25 13:45:00 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if there is no data with account id and survey id

--- src/data/protocols/db/survey/save-survey-result-repository.ts
+++ src/data/protocols/db/survey/save-survey-result-repository.ts
@@ -2,5 +2,5 @@ import { type SurveyResultModel } from '@/domain/models';
 import { type SaveSurveyResultModel } from '@/domain/usecases';
 
 export interface SaveSurveyResultRepository {
-  save: (surveyResultData: SaveSurveyResultModel) => Promise<SurveyResultModel>;
+  save: (surveyResultData: SaveSurveyResultModel) => Promise<SurveyResultModel | null>;
 }
--- src/domain/usecases/save-survey-result.ts
+++ src/domain/usecases/save-survey-result.ts
@@ -3,5 +3,5 @@ import { type SurveyResultModel } from '@/domain/models';
 export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>;
 
 export interface SaveSurveyResult {
-  save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel>;
+  save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel | null>;
 }
