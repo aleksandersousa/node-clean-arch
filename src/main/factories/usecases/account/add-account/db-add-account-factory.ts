cf2cd92b662beb43cfde0c3d3ff040d8939325ff
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-25 14:24:34 -0300

    refactor: organize project files

--- src/main/factories/usecases/account/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/account/add-account/db-add-account-factory.ts
@@ -1,7 +1,7 @@
 import { BcryptAdpater } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adapter';
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository';
 import { type AddAccount } from '@/domain/usecases';
-import { DbAddAccount } from '@/data/usecases/add-account/db-add-account';
+import { DbAddAccount } from '@/data/usecases/account/add-account/db-add-account';
 
 export const makeDbAddAccount = (): AddAccount => {
   const salt = 12;
