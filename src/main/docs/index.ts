99d827da8605c0b6e9714e39067513c23ea4c0e3
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-26 22:02:11 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,5 +1,6 @@
-import { loginPath } from './paths/login-path';
-import { accountSchema, loginParamsSchema } from './schemas';
+import { badRequest, serverError, unauthorized } from './components';
+import { loginPath } from './paths';
+import { accountSchema, errorSchema, loginParamsSchema } from './schemas';
 
 export default {
   openapi: '3.0.0',
@@ -20,5 +21,12 @@ export default {
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
+    error: errorSchema,
+  },
+
+  components: {
+    badRequest,
+    unauthorized,
+    serverError,
   },
 };
