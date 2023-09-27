0c85bcb679c8c95a8f3d2bddfc2f0db6fe8baf71
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-27 12:08:27 -0300

    feat: ensure DbLoadSurveyResult returns SurveyResultModel on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -4,7 +4,7 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor(private readonly loadSurveyResultRepositoryStub: LoadSurveyResultRepository) {}
 
   async load(surveyId: string): Promise<SurveyResultModel | null> {
-    await this.loadSurveyResultRepositoryStub.loadBySurveyId(surveyId);
-    return null;
+    const surveyResult = await this.loadSurveyResultRepositoryStub.loadBySurveyId(surveyId);
+    return surveyResult;
   }
 }
