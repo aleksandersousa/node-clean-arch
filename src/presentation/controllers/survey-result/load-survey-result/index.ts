2a3c6524feb81bb74ab9b887f511087098b038f9
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-27 14:40:05 -0300

    feat: ensure LoadSurveyResultController returns 403 if LoadSurveyById returns null

--- src/presentation/controllers/survey-result/load-survey-result/index.ts
+++ src/presentation/controllers/survey-result/load-survey-result/index.ts
@@ -1,3 +1,4 @@
 export * from '@/domain/usecases';
 export * from '@/presentation/protocols';
 export * from '@/presentation/helpers/http/http-helper';
+export * from '@/presentation/errors';
--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,4 +1,12 @@
-import { type Controller, type HttpRequest, type HttpResponse, ok, type LoadSurveyById } from '.';
+import {
+  type Controller,
+  type HttpRequest,
+  type HttpResponse,
+  ok,
+  type LoadSurveyById,
+  forbidden,
+  InvalidParamError,
+} from '.';
 
 export class LoadSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
@@ -6,7 +14,10 @@ export class LoadSurveyResultController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { surveyId } = httpRequest.params;
 
-    await this.loadSurveyById.loadById(surveyId);
+    const surveyResult = await this.loadSurveyById.loadById(surveyId);
+    if (!surveyResult) {
+      return forbidden(new InvalidParamError('surveyId'));
+    }
 
     return ok(null);
   }
