c4e9fac1ef969695c6a01ae57f672c84d85db988
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-27 12:08:45 -0300

    test: ensure DbLoadSurveyResult returns SurveyResultModel on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,7 +1,7 @@
 import { type LoadSurveyResultRepository } from '@/data/protocols';
 import { mockLoadSurveyResultRepository } from '@/data/test';
 import { DbLoadSurveyResult } from './db-load-survey-result';
-import { throwError } from '@/domain/test';
+import { mockSurveyResultModel, throwError } from '@/domain/test';
 
 type SutTypes = {
   sut: DbLoadSurveyResult;
@@ -33,5 +33,13 @@ describe('DbLoadSurveyResult Usecase', () => {
     const promise = sut.load('any_survey_id');
 
     await expect(promise).rejects.toThrow();
+  });
+
+  test('Should return SurveyResultModel on success', async () => {
+    const { sut } = makeSut();
+
+    const surveyResult = await sut.load('any_survey_id');
+
+    expect(surveyResult).toEqual(mockSurveyResultModel());
   });
 });
