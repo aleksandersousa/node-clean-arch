ad9eb754ae0f039c7d017ae03eb2a05c61f65177
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-27 15:18:29 -0300

    docs: add load survey result path to docs

--- src/main/docs/paths/survey-results-path.ts
+++ src/main/docs/paths/survey-results-path.ts
@@ -40,4 +40,36 @@ export const surveyResultsPath = {
       500: { $ref: '#/components/serverError' },
     },
   },
+
+  get: {
+    security: [{ apiKeyAuth: [] }],
+
+    tags: ['Respostas das Enquetes'],
+    summary: 'Api para consultar o resultado de uma enquete',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        description: 'id da pesquisa',
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: { $ref: '#/components/forbidden' },
+      500: { $ref: '#/components/serverError' },
+    },
+  },
 };
